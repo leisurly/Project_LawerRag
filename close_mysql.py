372855afaf4f0fdd197fc3f0be128a9da0411bd8
白白 <baibai@baibaideMacBook-Pro.local>
2025-05-02 11:11:29 +0800

    Remove all files

--- close_mysql.py
+++
@@ -1,4 +0,0 @@
-from connect_mysql import Connect_Mysql
-def Close_Mysql(conn, cursor):
-    cursor.close()
-    conn.close()
--- qdrant_key.py
+++
@@ -1,6 +0,0 @@
-from qdrant_client import QdrantClient
-
-qdrant_client = QdrantClient(
-    url="qrant client will aply",
-    api_key="YOR Qrdant API KEY",
-)
